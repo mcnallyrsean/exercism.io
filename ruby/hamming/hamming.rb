77f2de6810fcc520957890708b4740bc8756c967
Sean McNally <seanmcnally88@gmail.com>
2016-05-24 08:58:43 -0500

    Hamming problem

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,13 @@
-class Hamming
+class Hamming  
+  VERSION = 2
   def self.compute(strand1, strand2)
-    if strand1 == strand2
-      return 0
+    if strand1.length != strand2.length
+      raise ArgumentError, "Please provide strands of equal length"
+    else
+      s1 = strand1.chars
+      s2 = strand2.chars
+      comparison_array = s1.zip(s2).map {|x, y| x == y}
+      comparison_array.count(false)
     end
   end
 end
