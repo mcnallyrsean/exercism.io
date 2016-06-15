669c299aebcc1d70400ff0eb62252828d1f2e3f0
Sean McNally <seanmcnally88@gmail.com>
2016-06-15 15:22:52 -0500

    Refactored Bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,6 +1,6 @@
 class Bob
   def hey(remark)
-    if remark == remark.upcase && remark.gsub(/[^a-zA-Z]/, "").length > 0
+    if remark == remark.upcase && remark != remark.downcase
       "Whoa, chill out!"
     elsif remark[-1, 1] == "?"
       "Sure."
